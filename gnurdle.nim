8a0eecb42a3cca323ca3415384c1e154f065916c
fily-jpg <zeroad69@gmail.com>
2025-06-21 23:38:28 -0400

    started implementing word colorization. Word list TBD

--- gnurdle.nim
+++ gnurdle.nim
@@ -1,19 +1,29 @@
 import std/random
 import std/strformat
 import strutils
+import terminal # styledEcho(bgBlue, fgRed, "Color echo")
 
 randomize()
-const file = "words/words.txt"
-const word = readFile(file)
-const filesplit = splitLines(word)
-var randword = filesplit[rand(filesplit.len)]
+const fn = "words/words.txt"
+const file = readFile(fn)
+const filesplit = splitLines(file)
 
 echo(&"reading from {file}...")
 echo(&"loaded {filesplit.len} lines...");
-echo(&"random word: {randword}") # debug!
+
+proc colorizeword(word: string, target: string, status: int) = 
+    type Status = enum Green, Yellow, Gray
+    type Word = string
+    var statuses: array[5, Status]
+    var words: array[6, Word]
+    for i in 0 ..< word.len:
+        if word[i] == target[i]:
+            echo(i)
+
 proc main() = 
     echo("guess the word!")
-    echo(&"({randword})") # sanity checking, the word changes if incorrect, but last word still prints out???
+    let target = filesplit[rand(filesplit.len)]
+    echo(&"({target})") # FIXME: sanity checking, the word changes if incorrect, but last word still prints out???
     echo("_ _ _ _ _")
     var input = readLine(stdin)
     while true:
@@ -23,11 +33,11 @@ proc main() =
         elif input.len > 5:
             echo("your input is bigger than 5 characters!")
             break
-        elif input == randword:
-            echo(&"correct! the word is: {randword}")
+        elif input == target:
+            echo(&"correct! the word is: {target}")
             break
         else:
-            echo(&"incorrect! the word was: {randword}")
+            echo(&"incorrect! the word was: {target}")
             main()
 
-main()
+colorizeword("testy", "tesyy", 0)
