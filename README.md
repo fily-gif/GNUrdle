1768b7b70280ba7d68bc79e92dd897259111ac9e
fily-jpg <zeroad69@gmail.com>
2025-06-22 13:15:58 -0400

    final commit :oyes:

--- README.md
+++ README.md
@@ -2,4 +2,15 @@
 I'd just like to interject for a moment. What you're refering to as Worlde is in fact, GNU/Wordle, or as I've recently taken to calling it, GNU plus Wordle.
 ---
 ## What?
-GNUrdle (GNU/Wordle) is like Wordle, but with UNIX commands!
+GNUrdle (GNU/Wordle) is like Wordle, but with UNIX commands!<br>
+To use your own list, just add a `\n`-separated list of words into `words/unix.txt`.
+Current list was extracted using:
+```bash
+$ find $(echo $PATH | tr ':' ' ') -type f -executable -printf "%f\n" 2>/dev/null | grep -x '[a-z]\{5\}' | sort -u
+```
+(Pipe into `wc -l` to get the amount of words!)
+---
+## How to compile:
+```bash
+$ nim compile gnurdle.nim # (`$ nim compile -r gnurdle.nim` to run it right after!)
+```
--- gnurdle.nim
+++ gnurdle.nim
@@ -1,43 +1,68 @@
 import std/random
 import std/strformat
 import strutils
-import terminal # styledEcho(bgBlue, fgRed, "Color echo")
+import terminal
 
 randomize()
-const fn = "words/words.txt"
+const fn = "words/unix.txt"
 const file = readFile(fn)
 const filesplit = splitLines(file)
 
-echo(&"reading from {file}...")
-echo(&"loaded {filesplit.len} lines...");
+echo(&"reading from {fn}...")
+echo(&"loaded {filesplit.len} lines...")
 
-proc colorizeword(word: string, target: string, status: int) = 
-    type Status = enum Green, Yellow, Gray
-    type Word = string
-    var statuses: array[5, Status]
-    var words: array[6, Word]
-    for i in 0 ..< word.len:
+type Status = enum Green, Yellow, Gray
+
+proc colorizeword(word: string, target: string) = 
+    var statuses: array[5, Status] # why do arrays in nim have to be so weird omfg
+    var used: array[5, bool]
+
+    # first pass
+    for i in 0 ..< 5:
         if word[i] == target[i]:
-            echo(i)
+            statuses[i] = Green # correct
+            used[i] = true # adds to the array of used letters so that words like "refer" dont cause an edge-case
+        else:
+            statuses[i] = Gray # checked over in the second pass
+
+    # second pass
+    for i in 0 ..< 5:
+        if statuses[i] == Gray: # checking over every "invalid" letter so far...
+            for j in 0 ..< 5:
+                if not used[j] and word[i] == target[j]:
+                    statuses[i] = Yellow # wasnt used, exists in `target`
+                    used[j] = true
+                    break
+
+    for i in 0 ..< 5:
+        case statuses[i]
+        of Green: stdout.styledWrite(fgBlack, bgGreen, $word[i])
+        of Yellow: stdout.styledWrite(fgBlack, bgYellow, $word[i])
+        of Gray: stdout.styledWrite(fgWhite, bgRed, $word[i]) # supposed to be gray but i cba to rewrite yacpl into nim lmao
 
 proc main() = 
-    echo("guess the word!")
-    let target = filesplit[rand(filesplit.len)]
-    echo(&"({target})") # FIXME: sanity checking, the word changes if incorrect, but last word still prints out???
-    echo("_ _ _ _ _")
-    var input = readLine(stdin)
-    while true:
-        if input.len < 5:
-            echo("your input is fewer than 5 characters!")
-            break
-        elif input.len > 5:
-            echo("your input is bigger than 5 characters!")
-            break
-        elif input == target:
-            echo(&"correct! the word is: {target}")
-            break
-        else:
-            echo(&"incorrect! the word was: {target}")
-            main()
+    echo("Guess the UNIX command! (5 letters, 6 attempts)")
+    let target = filesplit[rand(filesplit.len)] # suprisingly fast (~100 micros!) for a file with... ~15k lines!
+    #echo(&"(DEBUG: {target})")
 
-colorizeword("testy", "tesyy", 0)
+    const maxAttempts = 6
+    var attempts = 0
+
+    while attempts < maxAttempts:
+        stdout.write(&"\n[Attempt {attempts+1}/6] Enter a word: ")
+        var input = readLine(stdin).toLowerAscii()
+
+        if input.len != 5:
+            echo("Input must be exactly 5 letters.")
+            continue
+
+        if input == target:
+            styledEcho(fgGreen, "Correct! The word is: ", target)
+            return
+        else:
+            colorizeword(input, target)
+            inc(attempts)
+
+    echo(&"Out of attempts! The correct word was: {target}")
+
+main()
